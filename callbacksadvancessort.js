f7c01dc4741e46d9a493b80bc059c8b75c7ecbe0
Aileen OBriengit config --global user.name Aileen <aileentobrien@gmail.com>
2016-10-07 18:55:42 +0000

    test fixing code on callbacksadvanced

--- callbacksadvancessort.js
+++ callbacksadvancessort.js
@@ -6,12 +6,12 @@ var students = [
 ];
 
 var sortThem = students.sort(function(a, b){
-    if (a.name > b.name)
+  if (a.name > b.name)
         return 1
-    if (a.name === b.name)
-    return b.age-a.age
+  if (a.name === b.name)
+   return b.age-a.age
 
-    return 0
+   return 0
 });
 
 
